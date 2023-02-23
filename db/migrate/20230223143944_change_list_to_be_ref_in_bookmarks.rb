6bb747ed1a395e94b6eaa19666077836490ad4f8
cristian fuhrich <fuehrich@gmail.com>
2023-02-23 14:48:07 +0000

    watchlist

---
+++ db/migrate/20230223143944_change_list_to_be_ref_in_bookmarks.rb
@@ -0,0 +1,6 @@
+class ChangeListToBeRefInBookmarks < ActiveRecord::Migration[7.0]
+    def change
+      remove_column :bookmarks, :list_id, :integer
+      add_reference :bookmarks, :list, foreign_key: true
+    end
+end
